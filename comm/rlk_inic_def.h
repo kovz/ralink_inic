42160b4905a065b9f18c455146f3c699f66f39a3
Vitalii Kovzik <vitalii.kovzik@gmail.com>
2017-03-07 18:04:34 +0100

    Removed custom wait definition

--- comm/rlk_inic_def.h
+++ comm/rlk_inic_def.h
@@ -72,39 +72,6 @@ extern s32 MC_CardIrq[MAX_NUM_OF_MULTIPLE_CARD];
 
 #endif // MULTIPLE_CARD_SUPPORT //
 
-#ifndef wait_event_interruptible_timeout
-#define __wait_event_interruptible_timeout(wq, condition, ret)		\
-do {									\
-	wait_queue_t __wait;						\
-	init_waitqueue_entry(&__wait, current);				\
-									\
-	add_wait_queue(&wq, &__wait);					\
-	for (;;) {							\
-		set_current_state(TASK_INTERRUPTIBLE);			\
-		if (condition)						\
-			break;						\
-		if (!signal_pending(current)) {				\
-			ret = schedule_timeout(ret);			\
-			if (!ret)					\
-				break;					\
-			continue;					\
-		}							\
-		ret = -ERESTARTSYS;					\
-		break;							\
-	}								\
-	current->state = TASK_RUNNING;					\
-	remove_wait_queue(&wq, &__wait);				\
-} while (0)
-
-#define wait_event_interruptible_timeout(wq, condition, timeout)	\
-({									\
-	long __ret = timeout;						\
-	if (!(condition))						\
-		__wait_event_interruptible_timeout(wq, condition, __ret); \
-	__ret;								\
-})
-#endif
-
 
 /* hardware minimum and maximum for a single frame's data payload */
 #define RLK_INIC_MIN_MTU		60	/* TODO: allow lower, but pad */
